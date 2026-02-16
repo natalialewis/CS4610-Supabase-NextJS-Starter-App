c14f132347ec6b8bcd26e8ffe59ed096f4e5acf2
Natalia Lewis <nattylewis11@gmail.com>
2026-02-15 22:34:03 -0700

    Added email to profiles table

---
+++ supabase/migrations/20260216052522_add_email_to_profiles.sql
@@ -0,0 +1,7 @@
+alter table "public"."profiles" add column "email" text not null;
+
+CREATE UNIQUE INDEX profiles_email_key ON public.profiles USING btree (email);
+
+alter table "public"."profiles" add constraint "profiles_email_key" UNIQUE using index "profiles_email_key";
+
+
